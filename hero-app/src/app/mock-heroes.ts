ee710fe4729836f31754652fc52cfda9a45cbcc7
Sam Coy <samueljoshuacoy@gmail.com>
2019-10-29 19:18:05 -0500

    Hero-detail component added

---
+++ hero-app/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { type: 'Earth', name: 'Big Dad' },
+  { type: 'Water', name: 'Small Dad' },
+  { type: 'Air', name: 'Blast Boy' },
+  { type: 'Earth', name: 'Git Gurl' },
+  { type: 'Shame', name: 'Magnificant Loser' },
+  { type: 'Fire', name: 'Random Ronald' },
+  { type: 'Fire', name: 'Donald Duck' },
+  { type: 'Earth', name: 'Mr Work it' },
+  { type: 'Chaos', name: 'Maglarky' },
+  { type: 'Quantum', name: 'Tim Stopper' }
+];
