405b27202d2bcaed129dec5527d09acb6c2dde43
Sam Coy <samueljoshuacoy@gmail.com>
2019-11-06 15:38:46 -0600

    Buttons backup

--- hero-app/src/app/hero-detail/hero-detail.component.ts
+++ hero-app/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import {MatButtonModule} from '@angular/material';
 
 @Component({
   selector: 'app-hero-detail',
@@ -16,7 +17,6 @@ export class HeroDetailComponent implements OnInit {
   }
 
   // Type Button --
-
   onClickMe(selectedtype: "herotype") {
     this.hero.type = selectedtype;
   }
