215fbb829d0c056204f4521ec5b5da78af2f9964
Sam Coy <samueljoshuacoy@gmail.com>
2019-11-08 21:30:38 -0600

    Service and Messages added. Needs to be fleshed out

---
+++ hero-app/src/app/message.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  messages: string[] = [];
+
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
+}
