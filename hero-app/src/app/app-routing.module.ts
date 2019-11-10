c89e23092f52e44c77cb4a9943f50495f6ce142b
Sam Coy <samueljoshuacoy@gmail.com>
2019-11-10 00:53:09 -0600

    Message routing added

--- hero-app/src/app/app-routing.module.ts
+++ hero-app/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { MessagesComponent }    from './messages/messages.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'messages', component: MessagesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent }
